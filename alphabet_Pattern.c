3337f22a4ec2948977180ada482a8e139a89edeb
Waseem <color.ui>
2024-02-13 19:32:42 +0530

    Five git file created

---
+++ alphabet_Pattern.c
@@ -0,0 +1,98 @@
+#include <stdio.h> 
+
+int main() 
+{ 
+	int rows = 5; 
+
+	// first loop for printing rows 
+	for (int i = 0; i < rows; i++) { 
+
+		// second loop for printing alphabets in each rows 
+		for (int j = 0; j <= i; j++) { 
+			printf("%c ", 'A' + j); 
+		} 
+		printf("\n"); 
+	} 
+	return 0; 
+}
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+alphabet_Pattern.c [unix] (05:29 01/01/1970)          0,0-1 All
+"alphabet_Pattern.c" [New]
+
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+~
+alphabet_Pattern.c [unix] (05:29 01/01/1970)          0,0-1 All
+"alphabet_Pattern.c" [New]
+
